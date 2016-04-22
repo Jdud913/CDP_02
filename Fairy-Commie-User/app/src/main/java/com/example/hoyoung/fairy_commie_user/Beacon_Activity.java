dd2f8ced7d6bf873dff3f8e13df75142e68e1c21
HoYoung <janghy913@naver.com>
2016-04-22 11:08:07 +0900

    블루투스 연결하는 부분을 alert dialog 형태로 구현해봄

--- Fairy-Commie-User/app/src/main/java/com/example/hoyoung/fairy_commie_user/Beacon_Activity.java
+++ Fairy-Commie-User/app/src/main/java/com/example/hoyoung/fairy_commie_user/Beacon_Activity.java
@@ -3,13 +3,12 @@ package com.example.hoyoung.fairy_commie_user;
 import android.app.Activity;
 import android.os.Bundle;
 
-
-public class Login_Activity extends Activity {
-
-    @Override
+/**
+ * Created by HoYoung on 2016-04-21.
+ */
+public class Beacon_Activity extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_intro);
     }
-
 }
